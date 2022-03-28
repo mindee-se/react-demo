5244a94b0a146bee1dc4f0296a82eac6b03bf56c
Ianaré Sévi <ianare@mindee.co>
2022-03-28 16:21:44 +0200

    A naive first version of a configurable UI

--- src/App.js
+++ src/App.js
@@ -7,33 +7,19 @@ import {getImagesFromPDF} from "react-mindee-js";
 import Viewer from "./components/Viewer/Viewer";
 import Form from "./components/Form/Form";
 import loaderGIF from "./assets/mindee-logo.gif";
+import config from "./config/config";
+let Config = config.getConfig();
 
 function App() {
     const [files, setFiles] = useState([])
     const [images, setImages] = useState([])
-    const [invoiceData, setInvoiceData] = useState({
-        locale: {value: ""},
-        total_incl: {value: ""},
-        total_excl: {value: ""},
-        invoice_date: {value: ""},
-        invoice_number: {value: ""},
-        due_date: {value: ""},
-        taxes: [],
-        supplier: {value: ""},
-        supplier_address: {value: ""},
-        customer_name: {value: ""},
-        customer_company_registration: {value: ""},
-        customer_address: {value: ""},
-        payment_details: [],
-        company_number: [],
-        total_tax: {value: ""},
-        line_items: [],
-    })
+    const [invoiceData, setInvoiceData] = useState(config.getInitialFields())
     const [shapes, setShapes] = useState([])
     const [loaded, setLoaded] = useState(false)
     const [activeFeature, setActiveFeature] = useState("")
 
     useEffect(() => {
+        document.title = Config.projectName + " Demo"
         // POST request using fetch inside useEffect React hook
         if (files.length > 0) {
             let data = new FormData()
@@ -43,7 +29,7 @@ function App() {
                 body: data
             };
 
-            fetch('http://localhost:8080/parse-doc', requestOptions)
+            fetch(Config.uploadURL, requestOptions)
                 .then(response => response.json())
                 .then(data => {
                     console.log(data)
